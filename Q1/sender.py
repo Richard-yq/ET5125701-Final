4cb347c1b475f66bae332477b4f2824ea1722767
Richard-yq <a2311496a@gmail.com>
2024-11-14 17:01:28 +0800

    Add sender(server) code

---
+++ Q1/sender.py
@@ -0,0 +1,17 @@
+import socket
+import time
+
+host = '192.168.88.15'
+port = 5406
+
+oSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+for i in range(1, 101):
+    message = "Packet " + str(format(i, '3d')) + " sended at t = " + str(format(time.time(), '.5f'))
+    
+    print(message)
+    
+    oSocket.sendto(message.encode('utf-8'), (host, port))
+    
+    time.sleep(0.1)
+
