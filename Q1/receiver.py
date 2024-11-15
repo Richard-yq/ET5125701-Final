c37f6a65228e2b2f3cf77b6d2d0a7ab013ae0db7
SoSonoFuriren <tobbyyang.taiwan@gmail.com>
2024-11-15 14:08:19 +0800

    Refactor receiver code to improve packet duration calculation and enhance output formatting

--- Q1/receiver.py
+++ Q1/receiver.py
@@ -8,22 +8,30 @@ port = 5407
 oSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 oSocket.bind((host, port))
 
-time_out_time = 10
-oSocket.settimeout(time_out_time)
+# time_out_time = 10
+# oSocket.settimeout(time_out_time)
 
+print('Receiver is ready')
+
+recv_time = 0
+counter = 1
 while True:
     try:
         Rec_message, Rec_address = oSocket.recvfrom(65535)
-        recv_time = time.time()
+        
 
         Rec_message = Rec_message.decode('utf-8')
         temp = Rec_message.split()
 
-        trans_time = float(temp[6])
-        Pocket_delay = (recv_time - trans_time) * 1000 
+        trans_time_next = time.time()
+        Pocket_duration = (trans_time_next - recv_time) * 1000 
 
-        print(Rec_message + ' | pocket delay = ' + str(format(Pocket_delay, '1.5f')) + ' ms')
+        if recv_time == 0:
+            Pocket_duration = 0
 
+        print(str(format(counter, '3d')) + '=>' + Rec_message + ' |  Packet duration = ' + str(format(Pocket_duration, '1.5f')) + ' ms')
+        recv_time = trans_time_next
+        counter += 1
     except socket.timeout:
         print('Time out')
         break
