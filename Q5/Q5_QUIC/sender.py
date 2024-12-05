2e09f72e9ca64c1cc8ca2ee07163e531ab8ac1cb
Richard-yq <a2311496a@gmail.com>
2024-12-05 10:40:56 +0800

    Q5_sender code (temp)

--- Q5/Q5_QUIC/sender.py
+++ Q5/Q5_QUIC/sender.py
@@ -1,30 +1,45 @@
-import socket
+import asyncio
 import time
+from aioquic.asyncio import connect
+from aioquic.quic.configuration import QuicConfiguration
+from aioquic.quic.events import HandshakeCompleted
+from aioquic.asyncio.protocol import QuicConnectionProtocol
 
-# sender:24
-# receiver:250
 sender_host = '192.168.88.24'
 sender_port = 5407
-
 receiver_ip = '192.168.88.250'
 receiver_port = 5500
-# create a socket object
-client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-# connect the client
-client.connect((receiver_ip, receiver_port))
+# 設定QUIC配置，這裡需要TLS憑證
+configuration = QuicConfiguration(is_client=True)
+configuration.load_cert_chain(certfile="//home//rapi//ET5125701-Final//Q5//Q5_QUIC//cert.pem", keyfile="//home//rapi//ET5125701-Final//Q5//Q5_QUIC//key.pem")  # 載入憑證
 
-for _ in range(1, 11):
-    for i in range(1, 11):
-        message = "Packet " + str(format(i, '3d')) + " sended at t = " + str(format(time.time(), '.5f'))
-        
-        print(message)
-        
-        client.sendto(message.encode('utf-8'), (receiver_ip, receiver_port))
-        
-        time.sleep(0.1)
-# receive data
-response = client.recv(65335)
+class MyQuicProtocol(QuicConnectionProtocol):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self._handshake_complete = False
+    
+    def handshake_complete(self):
+        """握手完成時觸發"""
+        self._handshake_complete = True
 
-print(response.decode())
-client.close()
+async def send_packets():
+    # 與QUIC伺服器建立連線
+    async with connect(receiver_ip, receiver_port, configuration=configuration, create_protocol=MyQuicProtocol) as protocol:
+        # 發送 10 個封包
+        for i in range(1, 11):
+            message = f"Packet {i:3d} sended at t = {time.time():.5f}"
+            print(message)
+            
+            # 使用 QUIC 的流來發送資料
+            protocol._quic.send_stream_data(0, message.encode('utf-8'))  # 發送資料流
+
+            # 讓伺服器有時間回應
+            await asyncio.sleep(0.1)
+        
+        # 等待回應
+        await protocol._quic.wait_for_ack()
+        print("Finished sending packets.")
+
+# 事件循環執行
+asyncio.run(send_packets())
