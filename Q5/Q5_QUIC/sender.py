55347c50c80390e711c8a6503aa6c0a4ce3d426a
SosonoFuriren <jcohaonuna@gmail.com>
2024-12-11 17:56:16 +0800

    Change the Q5 sender code

--- Q5/Q5_QUIC/sender.py
+++ Q5/Q5_QUIC/sender.py
@@ -1,45 +1,58 @@
 import asyncio
-import time
 from aioquic.asyncio import connect
-from aioquic.quic.configuration import QuicConfiguration
-from aioquic.quic.events import HandshakeCompleted
 from aioquic.asyncio.protocol import QuicConnectionProtocol
+from aioquic.quic.configuration import QuicConfiguration
+from aioquic.quic.events import HandshakeCompleted, StreamDataReceived
+import time
 
-sender_host = '192.168.88.24'
-sender_port = 5407
-receiver_ip = '192.168.88.250'
-receiver_port = 5500
-
-# 設定QUIC配置，這裡需要TLS憑證
-configuration = QuicConfiguration(is_client=True)
-configuration.load_cert_chain(certfile="//home//rapi//ET5125701-Final//Q5//Q5_QUIC//cert.pem", keyfile="//home//rapi//ET5125701-Final//Q5//Q5_QUIC//key.pem")  # 載入憑證
-
-class MyQuicProtocol(QuicConnectionProtocol):
+stream_length = 1
+packet_length = 1000000
+class ClientQuicProtocol(QuicConnectionProtocol):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self._handshake_complete = False
-    
-    def handshake_complete(self):
-        """握手完成時觸發"""
-        self._handshake_complete = True
+        self._stream_ids = []
 
-async def send_packets():
-    # 與QUIC伺服器建立連線
-    async with connect(receiver_ip, receiver_port, configuration=configuration, create_protocol=MyQuicProtocol) as protocol:
-        # 發送 10 個封包
-        for i in range(1, 11):
-            message = f"Packet {i:3d} sended at t = {time.time():.5f}"
-            print(message)
-            
-            # 使用 QUIC 的流來發送資料
-            protocol._quic.send_stream_data(0, message.encode('utf-8'))  # 發送資料流
+    def quic_event_received(self, event):
+        if isinstance(event, HandshakeCompleted):
+            max_bidi = self._quic._local_max_streams_bidi.value
+            max_uni = self._quic._local_max_streams_uni.value
+            print(f"Handshake completed with max_streams_bidi={max_bidi} and max_streams_uni={max_uni}")
+            for _ in range(stream_length):
+                stream_id = self._quic.get_next_available_stream_id(is_unidirectional=True)
+                self._stream_ids.append(stream_id)
+                # print(f"Generated stream ID: {stream_id}")
+            asyncio.create_task(self.send_data_on_streams())
+                
+        elif isinstance(event, StreamDataReceived):
+            print(f"Received: {event.data.decode()}")
 
-            # 讓伺服器有時間回應
-            await asyncio.sleep(0.1)
-        
-        # 等待回應
-        await protocol._quic.wait_for_ack()
-        print("Finished sending packets.")
+    async def send_data_on_streams(self):
+        tasks = []
+        count = 0
+        for stream_id in self._stream_ids:
+            # print("Sending data on stream", count+1, stream_id)
+            tasks.append(asyncio.create_task(self.send_data(stream_id, count + 1)))
+            count += 1
+        await asyncio.gather(*tasks)
 
-# 事件循環執行
-asyncio.run(send_packets())
+    async def send_data(self, stream_id, num):
+        for i in range((num - 1)* int(packet_length/stream_length) + 1, num * int(packet_length/stream_length) + 1):
+            message = f"Packet {i:6d}\n"
+            # message = "Group " + str(num) + " {" + ",".join([f"Packet {j}" for j in range((num-1)*int(packet_length/stream_length)+1 , num*int(packet_length/stream_length)+1)]) + "}"
+            self._quic.send_stream_data(stream_id, message.encode('utf-8'))
+            # time.sleep(0.0005)
+        self._quic.send_stream_data(stream_id, b"", end_stream=True)
+
+async def send_data():
+    configuration = QuicConfiguration(is_client=True)
+    configuration.max_stream_data = 65536
+    configuration.max_datagram_frame_size = 1452
+    configuration.verify_mode = False
+    configuration.initial_max_streams_bidi = stream_length  # Set max bidirectional streams
+    configuration.initial_max_streams_uni = stream_length   # Set max unidirectional streams
+    async with connect("192.168.88.250", 4433, configuration=configuration, create_protocol=ClientQuicProtocol) as protocol:
+        await protocol.wait_closed()
+
+if __name__ == "__main__":
+    import asyncio
+    asyncio.run(send_data())
